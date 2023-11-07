627e5ba5d11e55e64780e56bbaba6c825bfd2850
rachel <rachel.uiux@gmail.com>
2023-11-07 22:51:37 +0900

    build: update build configuration for vite
    - set the build output folder to dist
    - add 'base' in vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   include: ["src/**/*.ts*"],
+  base: "/money-calculator/",
   plugins: [react()],
 });
